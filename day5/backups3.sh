721a68fdedcf2100a7a4f8b5af277214342322c7
Naveed-Iqbal-Devops-Engineer <naveediqbalit@gmail.com>
2025-11-27 22:55:55 +0500

    Cloud Infrastructure Automation with Bash Scripting 🎯 Objective: To simulate real-world scenarios a Cloud Administrator faces daily by writing and executing Bash scripts for cloud instance setup, and automated backups using AWS CLI. 🏢 Industry Scenario: You’re a Cloud Admin at a health tech company using AWS. Launch servers automatically with a script — saves time. Backup files to AWS S3 nightly — keeps data safe and follows rules.

---
+++ day5/backups3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE_DIR="/home/naveed/shell-github/Day5"
+BACKUP_DIR="/backup"
+S3_BUCKET="s3://new-bucket-naveediqbal03"
+
+mkdir -p $BACKUP_DIR
+cp -r $SOURCE_DIR $BACKUP_DIR
+aws s3 sync $BACKUP_DIR $S3_BUCKET
+
+echo "✅ Backup completed successfully." 
