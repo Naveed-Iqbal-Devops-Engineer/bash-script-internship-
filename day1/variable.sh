19a75e14117b0aac4a635e4cceefe85b6b5c9f8b
Naveed-Iqbal-Devops-Engineer <naveediqbalit@gmail.com>
2025-11-27 21:58:26 +0500

    Bash Scripting Basics for Automation

---
+++ day1/variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This script demonstrates how to use variables in Bash
+
+# Define variables
+
+name="naveed"
+age=30
+course="Cloud Administration"
+
+echo "My name is: $name"
+echo "My age is: $age"
+echo "I am learning $course"
